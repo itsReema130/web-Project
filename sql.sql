47d15bc6e5ec8855bafac2e33ab869db5e092b3f
Arwa <arwa@MacBook-Pro-alkhas-b-Arwa-3.local>
2020-10-21 18:59:26 +0300

    add sql only

---
+++ sql.sql
@@ -0,0 +1,7 @@
+create table student
+(student_id number(3) primary key,
+ name varchar2(8),
+ phone_number varchar2(8));
+
+ insert into student values (111 ,'reema' , 'Riyadh');
+insert into student values (222 , 'Arwa', 'Dammam');
