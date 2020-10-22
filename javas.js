b7d8f8c93b6b1b798403c60b90fcbfd94f37a1df
Reema <reema.sh2211@gmail.com>
2020-10-22 19:54:18 +0300

    moving the cookie and using AOS done with the onclivk function

---
+++ javas.js
@@ -0,0 +1,16 @@
+// moving the cookie onclick
+function myMove() {
+    var elem = document.getElementById("cookies");
+    var pos = 0;
+    var id = setInterval(frame, 1);
+
+    function frame() {
+        if (pos == 2500) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.bottom = pos + 'px';
+        }
+    }
+}
